18a31dcba568582a49f83de46ea0911ff12b437f
Zhigang deng <deng_zg@hotmail.com>
2026-04-13 13:10:34 -0700

    change U20

--- src/components/files/FileList.js
+++ src/components/files/FileList.js
@@ -1,7 +1,7 @@
 import FileCard from "./FileCard";
 export default function FileList({ files, token }) {
   return (
-    <div className="space-y-6">
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
       {files.map(f => (
         <FileCard key={f.id} file={f} token={token} />
       ))}
